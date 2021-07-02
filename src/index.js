3961a486379221c4fb24b8481ccdc1181a5be3e4
Jerry Zhang <jerryzhang101@yahoo.com>
2021-07-02 14:00:17 -0400

    Added functions to create the homepage with only JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import pageLoad from "./pageLoad.js";
 
 pageLoad();
+
+//Cache DOM
+let home = document.querySelector("#home");
+let menu = document.querySelector("#menu");
+let contact = document.querySelector("contact");
+
+
+
+//Function to set everything in the DOM to hidden (for switching tabs)
+function hideAll() {
+    hours.style.visibility = "hidden";
+    title.style.visibility = "hidden";
+}
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -4,11 +4,102 @@ export default function pageLoad() {
     let body = document.querySelector("body");
     let content = document.querySelector("#content");
 
-    //Create initial DOM elements
-    const background = new Image();
+    //Create homepage DOM elements
+    const background = new Image(); //Background image
     background.src = "imgs/restaurant.jpg";
     background.classList.add("background");
 
+    const header = document.createElement("header"); //Header div
+    header.classList.add("header");
+
+    const navHolder = document.createElement("div"); //Div to hold navBar
+    navHolder.classList.add("navHolder");
+
+    const navBar = document.createElement("nav"); //Nav element
+    navBar.classList.add("navBar");
+
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("title");
+    titleDiv.id = "title";
+
+    const hours = document.createElement("div");
+    hours.classList.add("hours");
+    hours.id = "hours";
+
+    //Creates the elements to fill the navBar
+    let home = document.createElement("div");
+    home.id = "home";
+    let homeText = document.createElement("p");
+    homeText.textContent = "Home";
+    home.appendChild(homeText);
+    homeText.classList.add("navText")
+    let menu = document.createElement("div");
+    menu.id = "menu";
+    let menuText = document.createElement("p");
+    menuText.textContent = "Menu";
+    menu.appendChild(menuText);
+    menuText.classList.add("navText")
+    let contact = document.createElement("div");
+    contact.id = "contact";
+    let contactText = document.createElement("p");
+    contactText.textContent = "Contact";
+    contact.appendChild(contactText);
+    contactText.classList.add("navText")
+
+    //Append all elements to header/navHolder/navBar
+    navBar.appendChild(home);
+    navBar.appendChild(menu);
+    navBar.appendChild(contact);
+    navHolder.appendChild(navBar);
+    header.appendChild(navHolder);
+
+    //Create elements for the content div
+    let title = document.createElement("h1");
+    let message = document.createElement("h2");
+    let lineBreak = document.createElement("br");
+
+    //Fill text content for content elements
+    title.textContent = "The New Saloon";
+    message.textContent = "The modern hangout spot for everyone!";
+
+    //Append elements to titleDiv
+    titleDiv.appendChild(title);
+    titleDiv.appendChild(lineBreak);
+    titleDiv.appendChild(message);
+
+    //Append titleDiv to content
+    content.appendChild(titleDiv);
+
+    //Create elements for the restaurant operation hours
+    let sunday = document.createElement("p");
+    let monday = document.createElement("p");
+    let tuesday = document.createElement("p");
+    let wednesday = document.createElement("p");
+    let thursday = document.createElement("p");
+    let friday = document.createElement("p");
+    let saturday = document.createElement("p");
+
+    sunday.textContent = "Sunday:   12pm - 11am";
+    monday.textContent = "Monday:   12pm - 10am";
+    tuesday.textContent = "Tuesday: 12pm - 10am";
+    wednesday.textContent = "Wednesday: 12pm - 10am";
+    thursday.textContent = "Thursday:   12pm - 10am";
+    friday.textContent = "Friday:   12pm - 12am";
+    saturday.textContent = "Saturday:   12pm - 12am";
+
+    //Append times to hours div
+    hours.appendChild(sunday);
+    hours.appendChild(monday);
+    hours.appendChild(tuesday);
+    hours.appendChild(wednesday);
+    hours.appendChild(thursday);
+    hours.appendChild(friday);
+    hours.appendChild(saturday);
+
+    content.appendChild(hours);
+
+
     body.appendChild(background);
+    body.appendChild(header);
 
 }
